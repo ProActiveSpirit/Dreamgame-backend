25fe304bb3be301c5716816ad08bf99e9258fe02
proactivespirit <puddingbear0217@gmail.com>
2024-10-17 23:08:07 -0400

    fix(epay) fix epay healthcheck controller return status

--- src/controllers/epay/healthCheckController.js
+++ src/controllers/epay/healthCheckController.js
@@ -1,4 +1,4 @@
-const { checkHealth } = require('@services/epay/healthCheck');
+const { checkHealth } = require('@services/nintendo/healthCheck');
 
 async function healthCheckController(req, res) {
   try {
@@ -8,7 +8,7 @@ async function healthCheckController(req, res) {
       return res.status(503).json(healthStatus);
     }
 
-    res.status(200).json({ status: 'Healthy' });
+    res.status(200).json({ status: healthStatus.status });
   } catch (error) {
     console.log('Error in healthCheckController:', error);
     res.status(500).json({ errorCode: 'INTERNAL_SERVER_ERROR' });
--- src/controllers/nintendo/healthCheckController.js
+++ src/controllers/nintendo/healthCheckController.js
@@ -8,7 +8,7 @@ async function healthCheckController(req, res) {
       return res.status(503).json(healthStatus);
     }
 
-    res.status(200).json({ status: 'Healthy' });
+    res.status(200).json({ status: healthStatus.status });
   } catch (error) {
     console.log('Error in healthCheckController:', error);
     res.status(500).json({ errorCode: 'INTERNAL_SERVER_ERROR' });
