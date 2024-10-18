cf5171049dbc2df682008302d19610d23525acb4
proactivespirit <puddingbear0217@gmail.com>
2024-10-17 21:25:35 -0400

    feat(epay): add routers like nintendo

--- src/routes/epay/index.js
+++ src/routes/epay/index.js
@@ -1,8 +1,19 @@
 const express = require("express");
-const productRoutes = require("./productRoutes");
-
+const productRoutes = require("@routes/epay/productRoutes");
+const reservation = require("@routes/epay/reservationRoutes");
+const fulfillment = require("@routes/epay/fulfillmentRoutes");
+const codeStatus = require("@routes/epay/codestatusRoutes");
+const codeRevoke = require("@routes/epay/coderevokeRoutes");
+const healthcheck = require("@routes/epay/healthcheckRoutes");
+const combined = require("@routes/epay/combined-reservation-fulfillment-Routes");
 const router = express.Router();
 
 router.use(productRoutes);
+router.use(reservation);
+router.use(fulfillment);
+router.use(codeStatus);
+router.use(codeRevoke);
+router.use(healthcheck);
+router.use(combined);
 
 module.exports = router;
