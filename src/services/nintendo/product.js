c83aa9a663d2325770fbc2dac95e8dc6f01d7250
proactivespirit <puddingbear0217@gmail.com>
2024-11-06 05:21:34 -0500

    fix: update product  page database

--- src/services/nintendo/product.js
+++ src/services/nintendo/product.js
@@ -6,20 +6,21 @@ async function getProducts() {
   let allResults = [];
   let result = [];
   result = await prisma.nintendoData.findMany({});
-
+  console.log("products");
   try{
     allResults = result.map((game) => {
       return {
-          name: game.title,
-          stock: "Stock",
-          price: game.price_regular_f,
-          provider: "Nintendo",
-          region: game.region,
-          sku: game.product_code_txt[0],
-          publisher: game.publisher,
-          status: "Active",
-          createdAt: game.pretty_date_s
-        };
+        name: game.title,
+        stock: "Stock",
+        price: game.price_regular_f,
+        provider: "Nintendo",
+        region: game.region,
+        sku: game.product_code_txt[0],
+        publisher: game.publisher,
+        status: "Active",
+        createdAt: game.pretty_date_s,
+        region_sku: game.region + game.product_code_txt[0]
+      };
     });
   } catch (error) {
     console.error(
@@ -27,7 +28,7 @@ async function getProducts() {
     );
   }
 
-  return result;
+  return allResults;
 }
 
 module.exports = { getProducts };
