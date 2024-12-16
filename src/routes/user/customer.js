5f2e2ec2e5a4a7e79692d24d9041f7d3accc17af
proactivespirit <puddingbear0217@gmail.com>
2024-12-16 05:13:30 -0500

    feat: add customer table

--- src/routes/user/customer.js
+++ src/routes/user/customer.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const {
-    getCustomer
-} = require("../../controllers/user/getCustomer");
+  getCustomer,
+  createCustomer,
+} = require("../../controllers/user/customer");
 
 router.post("/getCustomer", getCustomer);
+router.post("/createCustomer", createCustomer);
 
 module.exports = router;
